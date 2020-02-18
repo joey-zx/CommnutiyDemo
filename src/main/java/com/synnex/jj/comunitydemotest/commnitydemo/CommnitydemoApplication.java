1883339e2a0a10ab8388cc0d6abf89d832f34e1c
joeyz <18380120510@163.com>
2020-02-18 10:53:22 +0800

    first Commit

---
+++ src/main/java/com/synnex/jj/comunitydemotest/commnitydemo/CommnitydemoApplication.java
@@ -0,0 +1,13 @@
+package com.synnex.jj.comunitydemotest.commnitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommnitydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommnitydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synnex/jj/comunitydemotest/commnitydemo/CommnitydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synnex.jj.comunitydemotest.commnitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommnitydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
